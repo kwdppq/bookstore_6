baed24092c5e4d6f2e22e0e8b6a2f8c1e30e8a5d
kwdppq <sgq970512@163.com>
2021-02-03 17:14:56 +0800

    加个BaseServlet工具类

---
+++ src/com/qfedu/util/BaseServlet.java
@@ -0,0 +1,33 @@
+package com.qfedu.util;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@WebServlet("/BaseServlet")
+public class BaseServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String methodName = request.getParameter("method");
+        Class<? extends BaseServlet> aClass = this.getClass();
+        try {
+            Method method = aClass.getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            method.invoke(this, request, response);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
